46c6eaa35c42648b3d9fb26f09f83e7d820b926a
Jestzer <james@buonopane.email>
2024-12-01 22:33:38 -0500

    feat: Does very basic things.

--- super-life-imposer/Program.cs
+++ super-life-imposer/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿if (args.Contains("-version"))
+{
+    Console.WriteLine("Version 0.0.1");
+    Environment.Exit(0);
+}
+
+string? playerInput = "broken-playerInput";
+
+Console.WriteLine("Hi.");
+Console.ForegroundColor = ConsoleColor.Yellow;
+while (true)
+{
+    Console.WriteLine("1. Hi. 2. It's been awhile.");
+    playerInput = Console.ReadLine();
+
+    if (playerInput == "1") { break; }
+    else { continue; }
+}
+
+Console.ResetColor();
